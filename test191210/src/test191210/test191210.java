12325d3e8771afb393407640d0bd7948414ecc20
17 <17@17-PC>
2019-12-10 14:24:21 +0900

    변수선언및 프린트문 작성

--- test191210/src/test191210/test191210.java
+++ test191210/src/test191210/test191210.java
@@ -1,5 +1,15 @@
 package test191210;
 
-public class test191210 {
+import java.util.Scanner;
 
+public class test191210 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int number;
+		int number1;
+		
+		System.out.println("¾È³ç");
+	}
+	
 }
+
